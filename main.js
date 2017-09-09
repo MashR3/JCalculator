3ab41f8b213805998038c3cadb4404590e12c7b1
MashR3 <michaelash17@googlemail.com>
2017-09-09 16:03:36 +0100

    Making sense. All is right with the world

--- main.js
+++ main.js
@@ -1,12 +1,34 @@
-var functio = prompt
+var operation = prompt
 
-var functio = prompt('Welcome to Java Calculator. Please select an operator:' + '\n ' + '\n [a] for Addition'+ '\n [s] for Subtraction' + '\n [d] for Division' + '\n [m] for Multiplication' + '\n [ad] for Advanced Operators');
+var operation = prompt('Welcome to Java Calculator. Please select an operator:' + '\n ' + '\n [a] for Addition'+ '\n [s] for Subtraction' + '\n [d] for Division' + '\n [m] for Multiplication' + '\n [ad] for Advanced Operators');
 
 
-if (functio === a){
-  prompt('Enter First Number');
-} else if (functio ===s) {
-  prompt('Enter First Number');
+if (operation === "a") {
+	var no1 = prompt('Enter First Number');
+	var no2 = prompt('Enter Second Number');
+	no1 = parseFloat(no1)
+	no2 = parseFloat(no2)
+	alert ("The sum is " + (no1 + no2) + ("!"))
+
+
+} else if (operation === "s") {
+	var no1 = prompt('Enter First Number');
+	var no2 = prompt('Enter number to subract');
+	no1 = parseFloat(no1)
+	no2 = parseFloat(no2)
+	alert ("The answer is " + (no1 - no2) "!")
+
+
+} else if (operation === "d") {
+	var no1 = prompt('Enter Number to be divided');
+	var no2 = prompt('Enter number to divide by');
+	no1 = parseFloat(no1)
+	no2 = parseFloat(no2)
+	alert ("The answer is " + (no1 / no2) + "!")
+
+
+
+
 } else if (functio === d) {
   prompt('Enter First Number');
 } else if (functio === m) {
@@ -24,3 +46,4 @@ if (functio === a){
 // if (functio = a|| s || d || m || ad) {
 // 	alert("Please choose a valid operator!") 
 // }
+
