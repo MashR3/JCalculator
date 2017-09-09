37c326c623ba7ff09f01fb3aa6c7e69fbabbadf9
MashR3 <michaelash17@googlemail.com>
2017-09-09 15:16:23 +0100

    fail calculator begins

---
+++ main.js
@@ -0,0 +1,26 @@
+var functio = prompt
+
+var functio = prompt('Welcome to Java Calculator. Please select an operator:' + '\n ' + '\n [a] for Addition'+ '\n [s] for Subtraction' + '\n [d] for Division' + '\n [m] for Multiplication' + '\n [ad] for Advanced Operators');
+
+
+if (functio === a){
+  prompt('Enter First Number');
+} else if (functio ===s) {
+  prompt('Enter First Number');
+} else if (functio === d) {
+  prompt('Enter First Number');
+} else if (functio === m) {
+	prompt('Enter First Number')
+}else {
+  alert('Please select a valid operator!');
+} 
+
+// if (functio = a) {
+// 	alert('wooo');
+// }
+
+// var function = (a || s || d || m || ad);
+
+// if (functio = a|| s || d || m || ad) {
+// 	alert("Please choose a valid operator!") 
+// }
